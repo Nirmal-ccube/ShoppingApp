f82936c929c15acfc0a627c0bd3c8c992d382149
Nirmal Patidar <npatidar@opencrowd.com>
2025-12-26 15:41:33 +0530

    feat: Item bought successful view in progress

---
+++ src/components/Modal/BuySuccess.tsx
@@ -0,0 +1,10 @@
+
+export default function BuySuccess() {
+    return (
+        <div className="gap-4">
+            <span className="material-icons-outlined">
+                check_circle_outline
+            </span>
+        </div>
+    )
+}
