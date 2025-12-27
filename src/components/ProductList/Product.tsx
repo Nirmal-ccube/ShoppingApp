8836d14e31f1acedcd32f66b10fe17eca2715685
Nirmal Patidar <nirmal.p@ccube.com>
2025-12-27 08:19:39 +0530

    fix: Product style added to common

--- src/components/ProductList/Product.tsx
+++ src/components/ProductList/Product.tsx
@@ -11,22 +11,18 @@ const rootProductCSS = "flex flex-col bg-gray-100 m-2 mx-4 rounded-md space-y-2 
 export default function Product({productModel, isTrendingProduct = false, onBuyTap}: ProductProps) {
 
     return (
-        <div className={`${rootProductCSS} ${isTrendingProduct ? 'h-50 w-40' : 'h-70 w-50'}`}>
-            <img src={productModel.imageUrl} className={`${isTrendingProduct ? 'w-20 h-20' : 'w-30 h-30'} object-contain`} alt={productModel.name}/>
+        <div className={`product-root ${isTrendingProduct ? 'product-trending' : 'product-normal'}`}>
+            <img src={productModel.imageUrl} className={isTrendingProduct ? 'product-img-trending' : 'product-img-normal'}  alt={productModel.name}/>
 
             <h2>{productModel.name}</h2>
 
             <h3>{productModel.price} {productModel.currency}</h3>
 
-
             { !isTrendingProduct &&
-                <button onClick={onBuyTap} className="bg-white text-m text-blue-600 border border-blue-600 rounded px-2 py-1 hover:bg-blue-600 hover:text-white transition-colors duration-300 w-max">
+                <button onClick={onBuyTap} className="product-buy-btn">
                     Buy Now
                 </button>
             }
-            
-            
-
         </div>
     )
 }
