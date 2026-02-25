4512e2e7b4ea9a9d1efcae29258f3fe5c4e7b538
Nirmal Patidar <nirmal.p@ccube.com>
2026-02-25 23:28:54 +0530

    responsive added for Product List

--- src/components/ProductList/Product.tsx
+++ src/components/ProductList/Product.tsx
@@ -6,17 +6,17 @@ type ProductProps = {
     onBuyTap?: () => void
 }
 
-export default function Product({productModel, isTrendingProduct = false, onBuyTap}: ProductProps) {
+export default function Product({ productModel, isTrendingProduct = false, onBuyTap }: ProductProps) {
 
     return (
         <div className={`product-root ${isTrendingProduct ? 'product-trending' : 'product-normal'}`}>
-            <img src={productModel.imageUrl} className={isTrendingProduct ? 'product-img-trending' : 'product-img-normal'}  alt={productModel.name}/>
+            <img src={productModel.imageUrl} className={isTrendingProduct ? 'product-img-trending' : 'product-img-normal'} alt={productModel.name} />
 
-            <h2>{productModel.name}</h2>
+            <h2 className="md:text-base md:px-2 md:py-1 sm:text-sm sm:px-1 sm:py-0.5 text-sm px-1 py-0.5">{productModel.name}</h2>
 
-            <h3>{productModel.price} {productModel.currency}</h3>
+            <h3 className="md:text-base md:px-2 md:py-1 sm:text-sm sm:px-1 sm:py-0.5 text-sm px-1 py-0.5">{productModel.price} {productModel.currency}</h3>
 
-            { !isTrendingProduct &&
+            {!isTrendingProduct &&
                 <button onClick={onBuyTap} className="product-buy-btn">
                     Buy Now
                 </button>
