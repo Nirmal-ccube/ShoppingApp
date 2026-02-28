4177e8d970801209a4bd87c53bfaa7d26aecf350
Nirmal Patidar <nirmal.p@ccube.com>
2026-02-28 15:33:58 +0530

    Side Menu Improved

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -3,7 +3,7 @@ import { SideMenuItemModel } from "./SideMenuItemModel";
 
 type SideMenuProp = {
     selectedItem: string,
-    onMenuItemTap: (title:string) => void
+    onMenuItemTap: (title: string) => void
 }
 
 const menuItems: SideMenuItemModel[] = [
@@ -12,30 +12,30 @@ const menuItems: SideMenuItemModel[] = [
     new SideMenuItemModel('Billing', '/billing')
 ]
 
-export default function SideMenu({selectedItem, onMenuItemTap } : SideMenuProp) {
+export default function SideMenu({ selectedItem, onMenuItemTap }: SideMenuProp) {
     return (
         //Side Menu Root
         <div className="sidemenu-root">
             {/* Top Menu Section */}
             <div className="sidemenu-header">
-                <img src='shopping_app_icon.svg' className="sidemenu-logo"/>
+                {/* <img src='shopping_app_icon.svg' className="sidemenu-logo"/> */}
                 <h2 className="sidemenu-title">
                     Shopping App
                 </h2>
-            </div>   
+            </div>
 
-            {/* Side Menu Items */}   
+            {/* Side Menu Items */}
             <div className="sidemenu-items">
-                {   menuItems.map((item: SideMenuItemModel, index: number) => {
-                        const selected = item.title === selectedItem
-                        return (
-                            <SideMenuItem 
-                                key={item.title} sideMenuItemModel={item} 
-                                onTap={ () => onMenuItemTap(item.title)} 
-                                selected={selected}
-                            />
-                        )
-                    })
+                {menuItems.map((item: SideMenuItemModel, index: number) => {
+                    const selected = item.title === selectedItem
+                    return (
+                        <SideMenuItem
+                            key={item.title} sideMenuItemModel={item}
+                            onTap={() => onMenuItemTap(item.title)}
+                            selected={selected}
+                        />
+                    )
+                })
                 }
             </div>
         </div>
