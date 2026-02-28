117e5a1ecf78d8bc1bc5a63c42552c0c911cfa7f
Nirmal Patidar <nirmal.p@ccube.com>
2026-02-28 09:12:22 +0530

    Fix: Currency symbol shown

--- src/components/ProductList/Product.tsx
+++ src/components/ProductList/Product.tsx
@@ -14,7 +14,7 @@ export default function Product({ productModel, isTrendingProduct = false, onBuy
 
             <h2 className="md:text-base md:px-2 md:py-1 sm:text-sm sm:px-1 sm:py-0.5 text-sm px-1 py-0.5">{productModel.name}</h2>
 
-            <h3 className="md:text-base md:px-2 md:py-1 sm:text-sm sm:px-1 sm:py-0.5 text-sm px-1 py-0.5">{productModel.price} {productModel.currency}</h3>
+            <h3 className="md:text-base md:px-2 md:py-1 sm:text-sm sm:px-1 sm:py-0.5 text-sm px-1 py-0.5">$ {productModel.price}</h3>
 
             {!isTrendingProduct &&
                 <button onClick={onBuyTap} className="product-buy-btn">
